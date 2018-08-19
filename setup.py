3b12de37a935801b8249cd66bb111b432c9c14fe
Léo Lam <leo@innovatetechnologi.es>
2018-08-19 14:26:41 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eventeditor",
-    version="1.0.2",
+    version="1.1.0",
     author="leoetlino",
     author_email="leo@leolam.fr",
     description="Event editor for Breath of the Wild",
