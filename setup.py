111dec7740b785af85273ee31cefbd53b98b2007
Léo Lam <leo@innovatetechnologi.es>
2018-10-05 23:40:15 +0200

    Use versioneer for versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import fastentrypoints
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="eventeditor",
-    version="1.2.1-1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="leoetlino",
     author_email="leo@leolam.fr",
     description="Event editor for Breath of the Wild",
