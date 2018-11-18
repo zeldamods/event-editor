7550b09b636154f9a6ea31ae8ac66b9cb2aaef41
Léo Lam <leo@innovatetechnologi.es>
2018-11-18 11:55:52 +0100

    Improve the AI class add dialog
    Now uses a line edit + list view instead of a combobox that makes it
    impossible to search for class names and is usually way too long.

--- eventeditor/actor_string_list_view.py
+++ eventeditor/actor_string_list_view.py
@@ -2,6 +2,7 @@ import typing
 
 import eventeditor.ai as ai
 import eventeditor.util as util
+from eventeditor.search_bar import SearchBar
 from evfl import EventFlow, Actor
 from evfl.common import StringHolder
 import PyQt5.QtCore as qc # type: ignore
@@ -72,6 +73,56 @@ class ActorStringListView(q.QWidget):
             builder(menu, idx)
         menu.exec_(self.sender().viewport().mapToGlobal(pos))
 
+class ActorAIClassAddDialog(q.QDialog):
+    def __init__(self, parent, model) -> None:
+        super().__init__(parent, qc.Qt.WindowTitleHint | qc.Qt.WindowSystemMenuHint)
+        self.setWindowTitle('Add an AI class')
+        self.setMinimumWidth(350)
+
+        ledit_hint = q.QLabel('Enter an AI class:')
+        ledit_hint.setAlignment(qc.Qt.AlignCenter)
+        self._ledit = q.QLineEdit()
+        list_hint = q.QLabel('or select one:')
+        list_hint.setAlignment(qc.Qt.AlignCenter)
+
+        self._list = q.QListView()
+        self._proxy_model = qc.QSortFilterProxyModel(self)
+        self._proxy_model.setSourceModel(model)
+        self._list.setModel(self._proxy_model)
+        self._list.setEditTriggers(q.QAbstractItemView.NoEditTriggers)
+        self._list.selectionModel().selectionChanged.connect(self._onSelectionChanged)
+        self._list.doubleClicked.connect(lambda idx: self.accept())
+
+        self._search_bar = SearchBar()
+        self._search_bar.hide()
+        self._search_bar.connectToFilterModel(self._proxy_model)
+        self._search_bar.addFindShortcut(self)
+
+        btn_box = q.QDialogButtonBox(q.QDialogButtonBox.Ok | q.QDialogButtonBox.Cancel);
+        btn_box.accepted.connect(self.accept)
+        btn_box.rejected.connect(self.reject)
+        layout = q.QVBoxLayout(self)
+        layout.addWidget(ledit_hint)
+        layout.addWidget(self._ledit)
+        layout.addWidget(list_hint)
+        layout.addWidget(self._list)
+        layout.addWidget(self._search_bar)
+        layout.addWidget(btn_box)
+
+    def accept(self) -> None:
+        if not self.getText():
+            q.QMessageBox.critical(self, self.windowTitle(), 'Please enter or select an AI class.')
+            return
+        super().accept()
+
+    def getText(self) -> str:
+        return self._ledit.text()
+
+    def _onSelectionChanged(self, selected, deselected) -> None:
+        if len(selected.indexes()) <= 0:
+            return
+        self._ledit.setText(selected.indexes()[0].data(qc.Qt.DisplayRole))
+
 class ActorActionListView(ActorStringListView):
     def __init__(self, parent, model, flow_data) -> None:
         super().__init__(parent, 'Actions', model, flow_data)
@@ -86,8 +137,10 @@ class ActorActionListView(ActorStringListView):
         name = self.actor.identifier.name
         aiprog = ai.load_aiprog(name)
         actions = list(aiprog.actions.keys()) if aiprog else []
-        text, ok = q.QInputDialog.getItem(self, 'Select an action', f'Action for {name}:', actions)
-        return text if ok else ''
+        add_dialog = ActorAIClassAddDialog(self, qc.QStringListModel(actions, self))
+        add_dialog.setWindowTitle(f'Add an action for {name}')
+        ret = add_dialog.exec_()
+        return add_dialog.getText() if ret else ''
 
 class ActorQueryListView(ActorStringListView):
     def __init__(self, parent, model, flow_data) -> None:
@@ -103,5 +156,7 @@ class ActorQueryListView(ActorStringListView):
         name = self.actor.identifier.name
         aiprog = ai.load_aiprog(name)
         queries = list(aiprog.queries.keys()) if aiprog else []
-        text, ok = q.QInputDialog.getItem(self, 'Select a query', f'Query for {name}:', queries)
-        return text if ok else ''
+        add_dialog = ActorAIClassAddDialog(self, qc.QStringListModel(queries, self))
+        add_dialog.setWindowTitle(f'Add a query for {name}')
+        ret = add_dialog.exec_()
+        return add_dialog.getText() if ret else ''
