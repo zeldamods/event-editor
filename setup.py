f6a97ba72e70962934765a914e99551f5322d472
Léo Lam <leo@innovatetechnologi.es>
2019-04-24 16:33:45 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3 :: Only",
     ],
     include_package_data=True,
-    install_requires=['evfl>=0.12.0', 'PyYAML~=3.12', 'aamp~=1.0', 'byml~=2.0', 'wszst_yaz0~=1.1.0'],
+    install_requires=['evfl~=1.1', 'PyYAML~=3.12', 'aamp~=1.0', 'byml~=2.0', 'wszst_yaz0~=1.1'],
     python_requires='>=3.6',
     entry_points={
         'gui_scripts': [
